ca6293a21a9df349ac50d64ca393dd4a75153965
NadirMarsiglia <nadir.marsigliababilonia@unicolombo.edu.co>
2024-04-21 18:59:32 -0500

    primer GUIA9

---
+++ src/main/java/com/mycompany/guias9/GUIAS9.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guias9;
+
+import igui.pantallas9;
+
+/**
+ *
+ * @author nadir
+ */
+public class GUIAS9 {
+
+    public static void main(String[] args) {
+     new pantallas9().setVisible(true);
+    }
+}
